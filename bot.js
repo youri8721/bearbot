bcfa02ac696a93c04769d66a2452f2eabe2f450c
youri8721 <35345410+youri8721@users.noreply.github.com>
2018-01-11 19:33:04 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('token');
+
+// THIS MUST BE THIS WAY
+client.login(process.env.BOT_TOKEN);
